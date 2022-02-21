4bfdea54d9e4bc390e0c31a10ea9801ccefb4371
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-02-21 12:44:01 +0100

    inicio maven project with spring

---
+++ mavenBasics/src/main/java/com/example/demo/MavenBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBasicsApplication.class, args);
+	}
+
+}
